53886e6ee6ee41853200b029c04641eaf689b2bd
Tanmay Bhosale <tanmaybhosale24@gmail.com>
2022-11-20 15:36:30 +0530

    add dp and stats to dashboard

--- pages/app/dashboard.tsx
+++ pages/app/dashboard.tsx
@@ -3,37 +3,64 @@ import checkImage from "../../assets/checked.png"
 import currentStreakImage from "../../assets/Current-streak.png"
 import wallClockImage from "../../assets/wall-clock.png"
 import Image from "next/image"
+import UserImage from "../../dummy/user.jpg"
 
 const Dashboard = ()=>{
     return(
         <WithNavbar>
-            <div className="mx-10 mt-3 w-full">
-                <section className="w-full h-10">
-                    <h2 className=" theme-text font-semibold text-xl">Hello, Evelyn Finley</h2>
-                </section>
-                <section className="w-full flex flex-row items-center justify-between mt-3">
-                    {/* active days */}
-                    <div className="px-2 py-1 flex flex-col items-center justify-center theme-text">
-                        <div><Image src={checkImage} alt="active days" height="50%" width="50%" /></div>
-                        <p className="text-2xl font-semibold">842</p>
-                        <p className="text-lg text-secondary font-semibold mt-2">Total Active Days</p>
-                        <p className="text-sm font-medium mt-1">May 24, 2020 - Presect</p>
+            <div className="mx-10 mt-3 w-full flex flex-row">
+                <div className="w-2/3">
+                    <section className="w-full h-10">
+                        <h2 className=" theme-text font-semibold text-xl">Hello, Evelyn Finley</h2>
+                    </section>
+                    <section className="w-full flex flex-col items-start justify-start p-2">
+                        {/* top icons */}
+                        <section className="w-full flex flex-row justify-around">
+                            {/* active days */}
+                            <div className="px-2 py-1 flex flex-col items-center justify-center theme-text">
+                                <div><Image src={checkImage} alt="active days" height="50%" width="50%" /></div>
+                                <p className="text-2xl font-semibold">842</p>
+                                <p className="text-lg text-secondary font-semibold mt-2">Total Active Days</p>
+                                <p className="text-sm font-medium mt-1">May 24, 2020 - Presect</p>
+                            </div>
+                            {/* cur streak */}
+                            <div className="px-2 py-1 flex flex-col items-center justify-center theme-text">
+                                <div className=" relative">
+                                    <Image src={currentStreakImage} alt="Current streak" width="100%" height="100%" />
+                                    <p className=" text-2xl font-semibold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">25</p>
+                                </div>
+                                <p className="text-lg text-secondary font-semibold mt-2">Current Streak</p>
+                                <p className="text-sm font-medium mt-1">Aug 23 - Sept 16</p>
+                            </div>
+                            {/* longest streak */}
+                            <div className="px-2 py-1 flex flex-col items-center justify-center theme-text">
+                                <div><Image src={wallClockImage} alt="active days" height="50%" width="50%" /></div>
+                                <p className="text-2xl font-semibold">25</p>
+                                <p className="text-lg text-secondary font-semibold mt-2">Longest Streak</p>
+                                <p className="text-sm font-medium mt-1">Aug 23 - Sept 16</p>
+                            </div>
+                        </section>
+                    </section>
+                </div>
+                <section className="w-1/3 p-2">
+                    <div className=" w-full h-auto flex flex-row items-center justify-end">
+                        <Image className=" rounded-full" width="50px" height="50px" src={UserImage} />
                     </div>
-                    {/* cur streak */}
-                    <div className="px-2 py-1 flex flex-col items-center justify-center theme-text">
-                        <div className=" relative">
-                            <Image src={currentStreakImage} alt="Current streak" width="100%" height="100%" />
-                            <p className=" text-2xl font-semibold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">25</p>
+                    <div className=" mt-7 w-full h-auto flex flex-row justify-between items-center">
+                        <div className="flex flex-col items-center">
+                            <p className="text-[#1DA1F2] font-bold text-sm">1,200</p>
+                            <p className="text-[#464447] font-normal text-sm">Followers</p>
                         </div>
-                        <p className="text-lg text-secondary font-semibold mt-2">Current Streak</p>
-                        <p className="text-sm font-medium mt-1">Aug 23 - Sept 16</p>
-                    </div>
-                    {/* longest streak */}
-                    <div className="px-2 py-1 flex flex-col items-center justify-center theme-text">
-                        <div><Image src={wallClockImage} alt="active days" height="50%" width="50%" /></div>
-                        <p className="text-2xl font-semibold">25</p>
-                        <p className="text-lg text-secondary font-semibold mt-2">Longest Streak</p>
-                        <p className="text-sm font-medium mt-1">Aug 23 - Sept 16</p>
+
+                        <div className="flex flex-col items-center">
+                            <p className="text-[#1DA1F2] font-bold text-sm">500</p>
+                            <p className="text-[#464447] font-normal text-sm">Following</p>
+                        </div>
+
+                        <div className="flex flex-col items-center">
+                            <p className="text-[#1DA1F2] font-bold text-sm">50</p>
+                            <p className="text-[#464447] font-normal text-sm">Tweets</p>
+                        </div>
                     </div>
                 </section>
             </div>
